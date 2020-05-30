827223ecde3fb235fba4ad627a8b45e7d83cba0e
jackichen <jackichen2006@gmail.com>
2020-05-30 15:37:13 +0800

    Update q95_gen_bst.cc

--- basic/algo/q95_gen_bst.cc
+++ basic/algo/q95_gen_bst.cc
@@ -7,7 +7,56 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
+public:
+    TreeNode* copyTree(TreeNode* root, int delta)
+    {
+        if (!root) return nullptr;
+
+        TreeNode* new_root = new TreeNode(root->val + delta);
+        new_root->left = copyTree(root->left, delta);
+        new_root->right = copyTree(root->right, delta);
+        return new_root;
+    }
+
+    vector<TreeNode*> generateTrees(int n) {
+        vector<TreeNode*> result;
+        if (n == 0)
+            return result;
+
+        vector<vector<TreeNode*>> trees;
+        trees.push_back({nullptr});
+        trees.push_back({new TreeNode(1)});
+
+        for (int i=2; i<=n; ++i) {
+            vector<TreeNode*> roots;
+            for (int root_val=1; root_val<=i; ++root_val) {
+                int left_count = root_val - 1, right_count = i - root_val;
+                for (auto left : trees[left_count]) {
+                    for (auto right : trees[right_count]) {
+                        TreeNode* root = new TreeNode(root_val);
+                        root->left = copyTree(left, 0);
+                        root->right = copyTree(right, root_val);
+                        roots.push_back(root);
+                    }
+                }
+            }
+            trees.push_back(roots);
+        }
+        return trees[n];
+    }
+};
+
+class Solution1 {
 public:
     void generateTreesHelper(int min, int max, vector<TreeNode*> &output)
     {
